aad9965bd559bcfc294b64398104e1cbeea7c1ec
Lee Hunter <whunter@vt.edu>
2016-11-30 19:27:39 +0000

    remove remote_files and uploaded_files

--- app/actors/curation_concerns/actors/work_actor.rb
+++ app/actors/curation_concerns/actors/work_actor.rb
@@ -3,6 +3,18 @@
 module CurationConcerns
   module Actors
     class WorkActor < CurationConcerns::Actors::BaseActor
+
+      protected
+
+        def apply_save_data_to_curation_concern(attributes)
+          attributes[:rights] = Array(attributes[:rights]) if attributes.key? :rights
+          remove_blank_attributes!(attributes)
+          attributes.delete(:remote_files) unless curation_concern.attributes[:remote_files]
+          attributes.delete(:uploaded_files) unless curation_concern.attributes[:uploaded_files]
+          curation_concern.attributes = attributes
+          curation_concern.date_modified = CurationConcerns::TimeService.time_in_utc
+        end
+
     end
   end
 end
