345c41ea73e1247187c407667ea1bd8dd3097f01
Lee Hunter <whunter@vt.edu>
2016-12-09 11:22:40 -0500

    add start server to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  require 'byebug/core'
+  Byebug.wait_connection = true
+  Byebug.start_server('localhost', 8081)
+  
 end
